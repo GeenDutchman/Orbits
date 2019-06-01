0ff82639517f7823323d2215d18b9a87d122a2a5
Drake Foltz <GeenDutchman@byu.net>
2019-06-01 19:35:08 -0400

    Data and plotting makes images

--- newton.py
+++ newton.py
@@ -1,48 +1,49 @@
-from  __future__ import print_function
+from __future__ import print_function
 import numpy as np
 from RK import RK4_Step
 
 
 def Keppler_RHS(t, y0, **kwargs):
-  """
-     Outputs the time derivatives of the position vector x and
-     velocity vector v according to Newton's law of gravitation.
-     The inputs are a time t, which is ignored, and a numpy vector y0.
-     y0 should contain 6 elements. The first three are the components
-     of x, the last three are the components of v. The output is a
-     numpy array with six elements corresponding to the time
-     derivatives of the components of x and v. The optional kwargs can
-     contain mass and Newton's constant parameters.
-   """
+    """
+       Outputs the time derivatives of the position vector x and
+       velocity vector v according to Newton's law of gravitation.
+       The inputs are a time t, which is ignored, and a numpy vector y0.
+       y0 should contain 6 elements. The first three are the components
+       of x, the last three are the components of v. The output is a
+       numpy array with six elements corresponding to the time
+       derivatives of the components of x and v. The optional kwargs can
+       contain mass and Newton's constant parameters.
+     """
 
-  x_vec = y0[0:3]  # position stored in first 3 elements
-  v_vec = y0[3:]   # velocity stored in last 3 elements
+    x_vec = y0[0:3]  # position stored in first 3 elements
+    v_vec = y0[3:]   # velocity stored in last 3 elements
 
-  r = np.linalg.norm(x_vec)
-  r3 = r**3
+    r = np.linalg.norm(x_vec)
+    r3 = r**3
 
-  if 'mass' in kwargs:
-    mass = kwargs['mass']
-  else:
-    mass = 1.0
+    if 'mass' in kwargs:
+        mass = kwargs['mass']
+    else:
+        mass = 1.0
 
-  if 'G' in kwargs:
-    G = kwargs['G']
-  else:
-    G = 1.0
- 
-  vdot = -mass * G / r3 * x_vec # Newton's law of gravitation
+    if 'G' in kwargs:
+        G = kwargs['G']
+    else:
+        G = 1.0
 
-  xdot = v_vec
+    vdot = -mass * G / r3 * x_vec  # Newton's law of gravitation
 
-  return np.concatenate((xdot, vdot))
+    xdot = v_vec
 
-### The Initial condition for the star orbiting a black hole
-### The black hole's mass is given by the 'mass',
-### Newton's constant by 'G', the initial position by
-### (x0, y0, z0), the initial velocity by (vx, vy, vz)
+    return np.concatenate((xdot, vdot))
 
-kwargs = { 'mass' : 1.0, 'G' : 1.0 }
+# The Initial condition for the star orbiting a black hole
+# The black hole's mass is given by the 'mass',
+# Newton's constant by 'G', the initial position by
+# (x0, y0, z0), the initial velocity by (vx, vy, vz)
+
+
+kwargs = {'mass': 1.0, 'G': 1.0}
 x0 = 1.0
 y0 = 0.0
 z0 = 0.0
@@ -63,8 +64,8 @@ Y = np.concatenate((initial_position, initial_velocity))
 
 
 while t < 10:
-  print (t, Y[0], Y[1])
+    print(Y[0], Y[1], Y[2])
 
-  ## The Runge-Kutta routine returns the new value of Y, t, and a
-  ## possibly updated value of dt
-  t, Y, dt = RK4_Step(t, Y, dt, Keppler_RHS, **kwargs)
+    # The Runge-Kutta routine returns the new value of Y, t, and a
+    # possibly updated value of dt
+    t, Y, dt = RK4_Step(t, Y, dt, Keppler_RHS, **kwargs)

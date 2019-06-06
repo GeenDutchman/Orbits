d8d0b18d1aa7c1e7c48c7c6c0994c49cdc6aa769
Drake Foltz <GeenDutchman@byu.net>
2019-06-06 18:56:31 -0400

    Got rid of velocity, just use omega
    Signed-off-by: Drake Foltz <GeenDutchman@byu.net>

--- binaryNewton.py
+++ binaryNewton.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import numpy as np
 from RK import RK4_Step, RK45_Step
 
+
 def calc_omega(mass, G, pos1, pos2):
     magnitude1 = np.linalg.norm(pos1)
     magnitude2 = np.linalg.norm(pos2)
@@ -50,21 +51,21 @@ def Keppler_Binary_RHS(t, y0, **kwargs):
         exit(2)
 
     BH1_x_vec = BH1[0:3]
-    BH1_v_vec = BH1[3:]
+    #BH1_v_vec = BH1[3:]
 
     BH2_x_vec = BH2[0:3]
-    BH2_v_vec = BH2[3:]
+    #BH2_v_vec = BH2[3:]
 
     omega, BH1r, BH2r = calc_omega(combined_BH_mass, G, BH1_x_vec, BH2_x_vec)
 
     # calculate the current position, but does not do the z coord??
-    BH1_x_vec[0] = BH1r * np.cos(BH1_v_vec[0] * omega * t)
-    BH1_x_vec[1] = BH1r * np.sin(BH1_v_vec[0] * omega * t)
-    BH1_x_vec[2] = 0 # don't do z...
+    BH1_x_vec[0] = BH1r * np.cos(omega * t)
+    BH1_x_vec[1] = BH1r * np.sin(omega * t)
+    BH1_x_vec[2] = 0  # don't do z...
 
     # calculate the current position, but does not do the z coord??
-    BH2_x_vec[0] = -1 * BH2r * np.cos(BH2_v_vec[0] * omega * t)
-    BH2_x_vec[1] = -1 * BH2r * np.sin(BH2_v_vec[0] * omega * t)
+    BH2_x_vec[0] = -1 * BH2r * np.cos(omega * t)
+    BH2_x_vec[1] = -1 * BH2r * np.sin(omega * t)
     BH2_x_vec[2] = 0  # don't do z...
 
     BH1_mass = combined_BH_mass / (BH_ratio + 1)
@@ -79,6 +80,9 @@ def Keppler_Binary_RHS(t, y0, **kwargs):
 
     acc_star = acc_star_1 + acc_star_2
     vel_star = star_v_vec
+
+    kwargs['bh1'] = BH1_x_vec
+    kwargs['bh2'] = BH2_x_vec
 
     return np.concatenate((vel_star, acc_star))
 
@@ -111,32 +115,38 @@ BH1x = 1.0
 BH1y = 0.0
 BH1z = 0.0
 
+'''
 BH1vx = 0.0
-BH1vy = -1.0
+BH1vy = 1.0
 BH1vz = 0.0
+'''
 
 initial_bh1_pos = np.array((BH1x, BH1y, BH1z), dtype=np.float64)
-initial_bh1_velocity = np.array((BH1vx, BH1vy, BH1vz), dtype=np.float64)
+#initial_bh1_velocity = np.array((BH1vx, BH1vy, BH1vz), dtype=np.float64)
 
-BH1 = np.concatenate((initial_bh1_pos, initial_bh1_velocity))
+BH1 = initial_bh1_pos # np.concatenate((initial_bh1_pos, initial_bh1_velocity))
 
 BH2x = -1.0
 BH2y = 0.0
 BH2z = 0.0
 
+'''
 BH2vx = 0.0
 BH2vy = -1.0
 BH2vz = 0.0
+'''
 
 initial_bh2_pos = np.array((BH2x, BH2y, BH2z), dtype=np.float64)
-initial_bh2_velocity = np.array((BH2vx, BH2vy, BH2vz), dtype=np.float64)
+#initial_bh2_velocity = np.array((BH2vx, BH2vy, BH2vz), dtype=np.float64)
 
-BH2 = np.concatenate((initial_bh2_pos, initial_bh2_velocity))
+BH2 = initial_bh2_pos # np.concatenate((initial_bh2_pos, initial_bh2_velocity))
 
 kwargs['bh1'] = BH1
 kwargs['bh2'] = BH2
 
 while t < 30:
+    BH1 = kwargs['bh1']
+    BH2 = kwargs['bh2']
     print(Y[1], Y[2], BH1[0], BH1[1], BH2[0], BH2[1])
 
     # The Runge-Kutta routine returns the new value of Y, t, and a

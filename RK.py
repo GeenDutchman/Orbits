3b9c3d600bd9026d86d6b422af6274c6a216472f
Rodrigul01 <rodrigul1994@hotmail.com>
2019-06-11 21:04:56 -0400

    This is a fresh test file

---
+++ RK.py
@@ -0,0 +1,117 @@
+import numpy as np
+""" 
+A very simple RK4 and RK45 implementation 
+"""
+
+
+DEFAULT_TOL = 1.0e-10
+DEFAULT_MAX_STEP_SIZE = 1.0e10
+
+def RK4_Step(xold, yold, dx, fxy, **kwargs):
+  """
+  Implement NON-adaptive RK4
+  Arguments:
+    xold : Old value of independent variable. Should be a scalar value
+           of type float, or np.float64.
+
+    yold : Old value (vector) of dependent variables in vector form.
+           Should be an np.array of type np.float64 or a scalar of
+           type float of np.float64.
+
+    dx : requested stepsize. Should be a scalar of type float or
+         np.float64
+
+    fxy: The RHS function. This function should accept arguments of
+        type xold, yold, and, optionally, **kwargs
+
+    **kwargs : a dictionary of optional parameters for fxy
+
+  Returns:
+    Updated values of x (independent variable), y (vector), dx
+    (always the same as the input dx)
+  """
+
+  k1 = fxy(xold, yold, **kwargs)
+  k2 = fxy(xold+ 0.5 * dx, yold + 0.5 * dx * k1, **kwargs)
+  k3 = fxy(xold+ 0.5 * dx, yold + 0.5 * dx * k2, **kwargs)
+  k4 = fxy(xold+ 1.0 * dx, yold + 1.0 * dx * k3, **kwargs)
+
+  xnew = xold + dx
+  ynew = yold + (dx / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
+  return xnew, ynew, dx
+
+
+
+# Adaptive RK45
+def RK45_Step(xold, yold, dx, fxy, tol=DEFAULT_TOL,
+    max_step_size=DEFAULT_MAX_STEP_SIZE, **kwargs):
+  """
+  Implement Adaptive RK45 (Runge-Kutta-Fehlberg)
+  Arguments:
+    xold : Old value of independent variable. Should be a scalar value
+           of type float, or np.float64.
+
+    yold : Old value (vector) of dependent variables in vector form.
+           Should be an np.array of type np.float64 or a scalar of
+           type float of np.float64.
+
+    dx : requested stepsize. Should be a scalar of type float or
+         np.float64
+
+    fxy: The RHS function. This function should accept arguments of
+        type xold, yold, and, optionally, **kwargs
+
+    tol : error tolerance. This is optional. Should be of
+          type float of np.float64
+
+    **kwargs : a dictionary of optional parameters for fxy
+
+  Returns:
+    Updated values of x (independent variable), y (vector), dx
+    (optimal step size)
+
+    If the timestep is rejected because dx is too large, then the
+    return values are xold, yold, and dxnew. Where dxnew is an
+    approximation to the optimal step size that should be used.
+  """
+
+
+  k1 = dx * fxy(xold, yold, **kwargs)
+  k2 = dx * fxy(xold + 1.0/4.0 * dx, yold + 1.0/4.0 * k1, **kwargs)
+  k3 = dx * fxy(xold + 3.0/8.0 * dx, yold + 3.0/32.0 * k1
+               + 9.0/32.0 * k2, **kwargs)
+  k4 = dx * fxy(xold + 12.0/13.0 * dx, yold + 1932.0/2197.0 * k1
+               - 7200.0/2197.0 * k2 + 7296.0/2197.0 * k3, **kwargs)
+  k5 = dx * fxy(xold + dx, yold + 439.0/216.0 * k1 - 8.0 * k2 
+               + 3680.0/513.0 * k3 - 845.0/4104.0 * k4, **kwargs)
+  k6 = dx * fxy(xold + 1.0/2.0 * dx, yold -8.0/27.0 * k1
+               + 2.0 * k2 - 3544.0/2565.0 * k3 + 1859.0/4104.0 * k4
+               - 11.0/40.0 * k5, **kwargs)
+
+  # higher order accuracy ynew
+  ynew = yold + 16.0/135.0 * k1 + 6656.0/12825.0 * k3 +\
+             28561.0/56430.0 * k4 -9.0/50.0 * k5 + 2.0/55.0 * k6
+
+  # lower order accuracy ynew
+  ynew4 = yold + 25.0/216.0 * k1 + 1408.0/2565.0 * k3 +\
+             2197.0/4104.0 * k4 -1.0/5.0 * k5
+
+  xnew = xold + dx
+
+  err= abs(np.array(ynew-ynew4)).max()
+
+  if err < tol * 1.0e-3:
+    dxnew = 2* dx
+  else:
+    dxnew = 0.9 * dx * (tol/err)**(0.25)
+
+  if dxnew > max_step_size:
+    dxnew = max_step_size 
+
+  # If the error is too big, we need to reject the step.
+  # Pass the old values of x and y, but a new smaller value of dx,
+  # to the calling function.
+  if err > tol:
+    return xold, yold, dxnew
+
+  return (xnew, ynew, dxnew)
---
+++ binaryNewton.py
@@ -0,0 +1,268 @@
+from __future__ import print_function
+import numpy as np
+from RK import RK4_Step, RK45_Step
+import sys, getopt
+
+
+def calc_omega(mass, G, pos1, pos2):
+    magnitude1 = np.linalg.norm(pos1)
+    magnitude2 = np.linalg.norm(pos2)
+    mag_sum = magnitude1 + magnitude2
+    dist = abs(mag_sum)
+    dist3 = dist ** 3
+
+    omega = np.sqrt((mass * G) / dist3)
+    return omega, magnitude1, magnitude2
+
+
+def Keppler_Binary_RHS(t, y0, **kwargs):
+    star_x_vec = y0[0:3]  # star position stored in first 3 elements
+    star_v_vec = y0[3:]  # star velocity stored in last 3 elements
+
+    # star position vector norm, such that
+    # star_r = (star_x**2 + star_y**2 + star_z**2)**1/2
+    # or the distance from the origin
+    # star_r = np.linalg.norm(star_x_vec)
+
+    if 'mass' in kwargs:
+        combined_BH_mass = kwargs['mass']
+    else:
+        combined_BH_mass = 1.0
+
+    if 'G' in kwargs:
+        G = kwargs['G']
+    else:
+        G = 1.0
+
+    if 'q' in kwargs:
+        BH_ratio = kwargs['q']
+    else:
+        BH_ratio = 1.0  # equal mass default
+
+    if 'bh1' in kwargs:
+        BH1 = kwargs['bh1']
+    else:
+        print("Must have black hole data!!")
+        exit(2)
+
+    if 'bh2' in kwargs:
+        BH2 = kwargs['bh2']
+    else:
+        print("Must have black hole information!!")
+        exit(2)
+
+    BH1_x_vec = BH1[0:3]
+    #BH1_v_vec = BH1[3:]
+
+    BH2_x_vec = BH2[0:3]
+    #BH2_v_vec = BH2[3:]
+
+    omega, BH1r, BH2r = calc_omega(combined_BH_mass, G, BH1_x_vec, BH2_x_vec)
+
+    # calculate the current position, but does not do the z coord??
+    BH1_x_vec[0] = BH1r * np.cos(omega * t)
+    BH1_x_vec[1] = BH1r * np.sin(omega * t)
+    BH1_x_vec[2] = 0  # don't do z...
+
+    # calculate the current position, but does not do the z coord??
+    BH2_x_vec[0] = -1 * BH2r * np.cos(omega * t)
+    BH2_x_vec[1] = -1 * BH2r * np.sin(omega * t)
+    BH2_x_vec[2] = 0  # don't do z...
+
+    BH1_mass = combined_BH_mass / (BH_ratio + 1)
+    # (-1 * combined_BH_mass * BH_ratio) / (BH_ratio + 1)
+    BH2_mass = combined_BH_mass - BH1_mass
+
+    # find the acceleration due to gravity from the respective black holes
+    acc_star_1 = (star_x_vec - BH1_x_vec) * (-1 * BH1_mass * G) / \
+        (np.linalg.norm(star_x_vec - BH1_x_vec) ** 3)
+    acc_star_2 = (star_x_vec - BH2_x_vec) * (-1 * BH2_mass * G) / \
+        (np.linalg.norm(star_x_vec - BH2_x_vec) ** 3)
+
+    acc_star = acc_star_1 + acc_star_2
+    vel_star = star_v_vec
+
+    kwargs['bh1'] = BH1_x_vec
+    kwargs['bh2'] = BH2_x_vec
+
+    return np.concatenate((vel_star, acc_star))
+
+def print_default():
+	print('\nThese are the default parameters:')
+	print('\nDefault X position of Star:\t\t\t\t2.0')
+	print('Default Y position of Star:\t\t\t\t0.0')
+	print('Default Z position of Star:\t\t\t\t0.2')
+	print('Default X component of Velocity of the Star:\t\t0.0')
+	print('Default Y component of Velocity of the Star:\t\t1.0')
+	print('Default Z component of Velocity of the Star:\t\t0.0')
+	print('Default time step: \t\t\t\t\t1.0e-2')
+	print('Default maximum run time: \t\t\t\t10\n')
+	
+
+def print_help():
+	print('\nThis program simulates a neutron star orbiting a binary black hole system')	
+	print('\npython3 binaryNewton.py')		
+	print('\n--star\t\t\t\tFlag to begin changing star parameters')
+	print('\t--x0,\t-x\t\tSets the x coordinate position of the star')
+	print('\t--y0,\t-y\t\tSets the y coordinate position of the star')
+	print('\t--z0,\t-z\t\tSets the z coordinate position of the star')
+	print('\t--vx0,\t-vx\t\tSets the Vx velocity component of the star')
+	print('\t--vy0,\t-vy\t\tSets the Vy velocity component of the star')
+	print('\t--vz0,\t-vz\t\tSets the Vz velocity component of the star')
+	print('--tstep,-ts\t\t\tSets the time step for the simulation data')
+	print('--tmax, -tm\t\t\tSets the maximum run time for the simulation data')
+	print('--default, -d\t\t\tShows the default parameters\n')
+
+def main(argv):
+	# The Initial condition for the star orbiting a black hole
+	# The black hole's mass is given by the 'mass',
+	# Newton's constant by 'G', the initial position by
+	# (x0, y0, z0), the initial velocity by (vx, vy, vz)
+
+	#Initializing default parameters
+	
+
+
+	kwargs = {'mass': 1.0, 'G': 1.0, 'q': 1.0}
+	x0 = 2.0
+	y0 = 0.0
+	z0 = 0.2
+
+	vx0 = 0.0
+	vy0 = 1.0
+	vz0 = 0.0
+
+	t = 0.0
+
+	tmax = 10
+
+	# dt is the timestep. The error will be proportional to dt**4
+	dt = 1.0e-2	
+
+	BH1x = 1.0
+	BH1y = 0.0
+	BH1z = 0.0
+
+	'''
+	BH1vx = 0.0
+	BH1vy = 1.0
+	BH1vz = 0.0
+	'''
+
+	BH2x = -1.0
+	BH2y = 0.0
+	BH2z = 0.0
+
+	'''
+	BH2vx = 0.0
+	BH2vy = -1.0
+	BH2vz = 0.0
+	'''
+
+
+	#Processing command line arguments
+	#This will possibly change some of the default values
+
+	i = 0
+	record_comment = False
+
+	if len(argv) == 0:
+		print('Running with default settings')
+
+	while i < len(argv): #while there are unprocessed arguments
+		if argv[i] == '--star':
+			i += 1
+			while i < len(argv): # while there are unprocessed star
+				#print('Star arguments')
+				if argv[i] == '--x0' or argv[i] == '-x':
+					i += 1
+					x0 = float(argv[i])
+					#print('X position  changed')
+				elif argv[i] == '--y0' or argv[i] == '-y':
+					i += 1
+					y0 = float(argv[i])
+					#print('Y position  changed')
+				elif argv[i] == '--z0' or argv[i] == '-z':
+					i += 1
+					z0 = float(argv[i])
+					#print('Z position  changed')				
+				elif argv[i] == '--vx0' or argv[i] == '-vx':					
+					i += 1
+					vx0 = float(argv[i])
+					#print('Velocity x vector  changed')
+				elif argv[i] == '--vy0' or argv[i] == '-vy':					
+					i += 1
+					vy0 = float(argv[i])
+					#print('Velocity y vector  changed')
+				elif argv[i] == '--vz0' or argv[i] == '-vz':					
+					i += 1
+					vz0 = float(argv[i])
+					#print('Velocity z vector  changed')
+				else: 
+					i -= 1
+					break
+				i += 1
+		elif argv[i] == '--tstep' or argv[i] == '-ts':
+			i += 1
+			dt = float(argv[i])
+			#print('Time step changed')
+		elif argv[i] == '--tmax' or argv[i] == '-tm':
+			i += 1
+			tmax = float(argv[i])
+			#print('Maximum run time changed')	
+		elif argv[i] == '--help' or argv[i] == '-h':
+			print_help()
+			exit(0)
+		elif argv[i] == '--default' or argv[i] == '-d':
+			print_default()
+			exit(0)
+		elif argv[i] == '-r' or argv[i] == '--record':
+			record_comment = True
+		else:
+			print('\n', argv[i], ' is not an option!!')
+			print_help()
+			exit(1)
+		i += 1
+	
+
+	#Calculate initial star position 
+	initial_position = np.array((x0, y0, z0), dtype=np.float64) 
+
+	#Calculate initial star velocity
+	initial_velocity = np.array((vx0, vy0, vz0), dtype=np.float64)
+
+	#Concatanate star parameters
+	Y = np.concatenate((initial_position, initial_velocity)) 
+
+	#Puts blach hole 1 position parameters into an array
+	initial_bh1_pos = np.array((BH1x, BH1y, BH1z), dtype=np.float64)
+
+	BH1 = initial_bh1_pos
+
+	#Puts black hole 2 position parameters into an array
+	initial_bh2_pos = np.array((BH2x, BH2y, BH2z), dtype=np.float64)
+
+	BH2 = initial_bh2_pos
+
+	kwargs['bh1'] = BH1
+	kwargs['bh2'] = BH2
+	#kwargs['q'] = 0.5
+
+	if record_comment:
+		print('# Star Position: x:', x0, ' y:', y0, ' z:', z0)
+		print('# Star Velocity Components: vx0: ', vx0, ' vy0:', vy0, ' vz0:', vz0)
+		print('# Time Step:', dt, '\tRun Time max:', tmax)
+		print('')
+
+	while t < tmax:
+		BH1 = kwargs['bh1']
+		BH2 = kwargs['bh2']
+		print(t, Y[0], Y[1], Y[2],BH1[0], BH1[1], BH1[2], BH2[0], BH2[1], BH2[2])
+
+		# The Runge-Kutta routine returns the new value of Y, t, and a
+		# possibly updated value of dt
+		t, Y, dt = RK4_Step(t, Y, dt, Keppler_Binary_RHS, **kwargs)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+

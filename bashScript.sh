a0121494c023972a142c572d9e47aaee57e2a7af
Drake Foltz <GeenDutchman@byu.net>
2019-06-15 17:05:59 -0400

    Script can now do logs
    Signed-off-by: Drake Foltz <GeenDutchman@byu.net>

--- bashScript.sh
+++ bashScript.sh
@@ -1,44 +1,68 @@
 #!/bin/bash
 
+# only print to stdout and stderr if not equal to "0"
+NO_TEE="0"
+
+err_file="errors.log.txt"
+#err_flag=0
+log_file="hist.log.txt"
+#log_flag=0
+
+function tee_print() {
+    if  { { [ "$1" == "-nt" ] || [ "$1" == "--no-tee" ]; } && shift; } || [ "$NO_TEE" != "0" ]; then
+        # echo "no tee"
+        if [ "$1" == "-p" ] || [ "$1" == "--prob" ]; then
+            shift 
+            echo $@ >> /dev/stderr
+        else
+            echo $@
+        fi
+    else # just echo normalish
+        # echo "normal echo"
+        if [ "$1" == "-p" ] || [ "$1" == "--prob" ]; then     
+            shift
+            date_string = $(date)
+            echo -n date_string >> $err_file
+            echo $@ | tee -a $log_file $err_file
+        else
+            echo $@ | tee -a $log_file
+        fi
+    fi
+
+}
+
 function file_exists() {
-    echo -n "Checking for \"$1\" in this directory..."
+    tee_print -nt -n "Checking for \"$1\" in this directory..."
     exists=$( ls | grep -c $1 )
     if [ $exists == 0 ]; then
-        echo "not found!!"
+        tee_print -nt "not found!!"
         return 1
     fi
-    echo "found."
+    tee_print -nt "found."
 }
 
 function pre_checks {
-    # binaryNewton_exists=$( ls | grep -c 'binaryNewton.py' )
-    # if [ $binaryNewton_exists == 0 ]; then
-    #     (>&2 echo 'This script cannot be run in this directory.') 
-    #     (>&2 echo 'Run in a directory with "binaryNewton.py"')
-    #     exit 1
-    # fi
-
     declare -a files=("binaryNewton.py" "RK.py")
     for file_name in ${files[@]}
     do
         file_exists $file_name
         if [ $? != 0 ]; then
-            (>&2 echo 'Please run in the directory with this file')
+            tee_print -nt -p "Please run in the directory with '$file_name'"
             exit 1
         fi
     done
 
     python_exists=$( which python3 )
     if [ $? != 0  ]; then
-        (>&2 echo 'This script requires "python3" to be installed.')
-        (>&2 echo 'Please install "python3" and then rerun.')
+        tee_print -p 'This script requires "python3" to be installed.'
+        tee_print -p 'Please install "python3" and then rerun.'
         exit 1
     fi
 }
 
 function display_animation {
     if [ -e 'binary1.dat' ]; then
-        echo 'Displaying animation'
+        tee_print -nt 'Displaying animation'
         BIN_LEN=$(wc -l < binary1.dat)
         NUM_COMMENTS=$(grep -c "#" binary1.dat)
         # echo $BIN_LEN
@@ -46,7 +70,7 @@ function display_animation {
         # echo $((BIN_LEN - NUM_COMMENTS))
         gnuplot -c 'moviePlot.plt' $((BIN_LEN - NUM_COMMENTS))
     else
-        (>&2 echo 'No data file')
+        tee_print -p 'No data file for the animation!'
     fi
 
     # how to make a movie
@@ -56,27 +80,28 @@ function display_animation {
 
 function display_plate {
     if [ -e 'binary1.dat' ]; then
-        echo "Displaying plate"
+        tee_print -nt "Displaying plate"
         gnuplot -c 'platePlot.plt'
     else
-        (>&2 echo 'No data file')
+        tee_print -p 'No data file for the plate!'
     fi
 }
 
 
 function main {
+    date >> $log_file
     write_success=$( python3 binaryNewton.py $@ > binary1.dat )
     if [ $? != 0 ]; then
-        (>&2 echo 'An error occured during python execution!')
+        tee_print -p 'An error occured during python execution!'
         err_msg=$(grep '!!' binary1.dat)
-        (>&2 echo $err_msg)
-        (>&2 echo 'Logging the error')
-        date >> errors.log.txt
-        echo $err_msg >> errors.log.txt
-        (>&2 echo 'Removing the errant file')
+        tee_print -nt 'Logging the error'
+        tee_print -p $err_msg        
+        # date >> errors.log.txt
+        # echo $err_msg >> errors.log.txt
+        tee_print -nt 'Removing the errant file'
         rm ./binary1.dat
     else
-        echo 'The data was produced and written successfully.'
+        tee_print 'The data was produced and written successfully.'
         display_animation
         display_plate
     fi

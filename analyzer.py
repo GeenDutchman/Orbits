b6b55343b27c4dd27e6e9cf8697a6a562e289b23
Drake Foltz <GeenDutchman@byu.net>
2019-06-26 09:03:56 -0400

    Works, but needs to not do any plates...

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import numpy as np
+import subprocess
 
-for i in range(0.0, 4.0, 0.2):
+
+for i in range(0, 4, 2):
+    return_code = subprocess.run(
+        ["./bashScript.sh", "--star", "-x",  "100", "-vy", "0.05", "-45", "--tmax", "25000", "--sep", (i*0.1).__str__()], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 

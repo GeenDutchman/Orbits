dae281c26912332a2f83b5bc071fb4b17b7fff98
Drake Foltz <GeenDutchman@byu.net>
2019-07-05 07:25:04 -0400

    Working on analyzer
    Signed-off-by: Drake Foltz <GeenDutchman@byu.net>

--- analyzer.py
+++ analyzer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
 import numpy as np
-import subprocess
+import subprocess as sbp
 
-
-for i in range(0, 42, 2):
+for i in range(100, 103, 1):
     # this is meant to run a series of values
     # as a process in the background
     # also make sure to not run any plates or the movie
-    return_code = subprocess.run(
-        ["./bashScript.sh", "--star", "-x",  "100", "-vy", "0.0" ,"-vz", "-0.05", "-45", "--tmax", "25000", "--sep", (i*0.1).__str__()], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    result = sbp.run(
+        ["./grScript.sh", "--star", "-x",  "200", "-vy", "0.05" ,"-45", "--tmax", "9e6", "--sep -x", (i).__str__()], stdout=sbp.DEVNULL, stderr=sbp.DEVNULL)
 
+# Alert the user that the code has finished
+result = sbp.run(["which mail"], shell=True)
+if result.returncode == 0:
+    result = sbp.run(["mail -s 'Test run' GeenDutchman@mail.rit.edu larreu@rit.edu <<< 'The test is complete'"], shell=True)
+result = sbp.run(["which espeak"], shell=True, stdout=sbp.DEVNULL)
+if result.returncode == 0:
+    result = sbp.run(["espeak", "'This run is complete.'"], shell=True)
+print(result)
+# if neither goes off....oh well

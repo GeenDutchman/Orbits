ef56c036b047d4e1ee2ffae21cf1885e41e27bad
larreu <rodrigul1994@hotmail.com>
2019-07-08 12:12:27 -0400

    Changes opt to account for each orbital shift,see change in precession

--- opt.py
+++ opt.py
@@ -12,8 +12,8 @@ def find_residual(phi_old, r_old, phi_new, r_new):
         residuals.append(r_old[i] - r_interp)
     return np.linalg.norm(residuals)
         
-def shifter(phi_column, epsilon):
-    phi_shifted = [x - (2*np.pi + epsilon) for x in phi_column]
+def shifter(phi_column, epsilon, nOrbits):
+    phi_shifted = [x - (2*np.pi*nOrbits + epsilon) for x in phi_column]
     return phi_shifted
 
 def copy_piece(phi_column, r_column, start_cut, end_cut):
@@ -37,31 +37,33 @@ def extractor(data):
         
 data = np.loadtxt('binary1.dat', dtype=np.float64)
 phi_original, r_original = extractor(data)
-phi_old, r_old = copy_piece(phi_original, r_original, np.pi, 3*np.pi)
+phi_old, r_old = copy_piece(phi_original, r_original, 2*np.pi - np.pi, 2*np.pi + np.pi)
 
-def opt(epsilon):
+def opt(epsilon, *args):
+    nOrbits = args[0]
     epsilon = epsilon[0]
-    phi_shifted = shifter(phi_original, epsilon)
-    phi_new, r_new = copy_piece(phi_shifted, r_original, (np.pi/2), ((7*np.pi)/2))
+    phi_shifted = shifter(phi_original, epsilon, nOrbits)
+    phi_new, r_new = copy_piece(phi_shifted, r_original,2*np.pi - 1.5*np.pi,2*np.pi + 1.5*np.pi)
     if phi_old[-1] >= phi_new[-1]:
         raise ValueError('The data needs to have more orbits!!')
     return find_residual(phi_old, r_old, phi_new, r_new)
 
-try:
-    num_orbits = phi_original[-1] / (2 * np.pi)
-    print('The star does', num_orbits, 'orbits.')
-    result = minimize(opt, 0.005, method='Powell')
-    if result.success:
-        print(result.message)
-        print('Angle of precession:', result.x)
-        print()
-    else:
-        print('An error occured:')
-        print(result.message)
-        print()
+num_orbits = phi_original[-1] / (2 * np.pi)
+print('The star does', num_orbits, 'orbits.')
+for nOrbits in range(1, int(num_orbits)):
+    try:
+        result = minimize(opt, 0.005 , method='Powell', args=(nOrbits,))
+        if result.success:
+            print(result.message)
+            print('For orbit:',nOrbits + 1,'Angle of precession:', result.x)
+            print()
+        else:
+            print('An error occured:')
+            print(result.message)
+            print()
+            exit(1)
+    except ValueError as e:
+        print('An exception was caught:')
+        print(e)
         exit(1)
-except ValueError as e:
-    print('An exception was caught:')
-    print(e)
-    exit(1)
 

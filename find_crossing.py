4652584a47b63fc6e8a0492a4ffc9aa5847a81af
larreu <rodrigul1994@hotmail.com>
2019-07-10 14:46:48 -0400

    Made further alterations to find_crossing to consider erros

--- find_crossing.py
+++ find_crossing.py
@@ -1,12 +1,27 @@
 def crossing_finder(x_lst, y_lst):
     master_x_cross = []
     master_y_cross = []
-    for i in range(len(y_lst)):
-        if y_lst[i]*y_lst[i+1] <= 0:
-            x_cross_lst = [x_lst[i-1], x_lst[i], x_lst[i+1], x_lst[i+2]]  
-            y_cross_lst = [y_lst[i-1], y_lst[i], y_lst[i+1], y_lst[i+2]]
-            master_x_cross.append(x_cross_lst)
-            master_y_cross.append(y_cross_lst)
+    i = 1
+    while i < len(y_lst) - 2:#Makes sure it doesn't do an out of bounds index
+            if y_lst[i]*y_lst[i+1] <= 0:
+                x_cross_lst = [x_lst[i-1], x_lst[i], x_lst[i+1], x_lst[i+2]]  
+                y_cross_lst = [y_lst[i-1], y_lst[i], y_lst[i+1], y_lst[i+2]]
+                master_x_cross.append(x_cross_lst)
+                master_y_cross.append(y_cross_lst)
+        
+                if y_lst[i+1] == 0:#Makes sure it doesn't add repeating sequences
+                    i += 2  
+                    print('second:',i)
+            i += 1
     return master_x_cross, master_y_cross
+
+#Goals:
+#Make sure it skips every odd value, only considers even values
+
+
+
+
+
+
 
 

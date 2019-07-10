e1d8ffe33f51c23f9dcc9f3b9768f917d05e24a9
larreu <rodrigul1994@hotmail.com>
2019-07-10 15:29:11 -0400

    find crossing should be working now

--- find_crossing.py
+++ find_crossing.py
@@ -2,21 +2,24 @@ def crossing_finder(x_lst, y_lst):
     master_x_cross = []
     master_y_cross = []
     i = 1
+    full_orbit = False
     while i < len(y_lst) - 2:#Makes sure it doesn't do an out of bounds index
             if y_lst[i]*y_lst[i+1] <= 0:
-                x_cross_lst = [x_lst[i-1], x_lst[i], x_lst[i+1], x_lst[i+2]]  
-                y_cross_lst = [y_lst[i-1], y_lst[i], y_lst[i+1], y_lst[i+2]]
-                master_x_cross.append(x_cross_lst)
-                master_y_cross.append(y_cross_lst)
-        
+                if full_orbit:
+                    x_cross_lst = [x_lst[i-1], x_lst[i], x_lst[i+1], x_lst[i+2]]  
+                    y_cross_lst = [y_lst[i-1], y_lst[i], y_lst[i+1], y_lst[i+2]]
+                    master_x_cross.append(x_cross_lst)
+                    master_y_cross.append(y_cross_lst)
+
                 if y_lst[i+1] == 0:#Makes sure it doesn't add repeating sequences
                     i += 2  
                     print('second:',i)
+
+                full_orbit = not full_orbit #Make sure it skips every other value
             i += 1
     return master_x_cross, master_y_cross
 
-#Goals:
-#Make sure it skips every odd value, only considers even values
+
 
 
 

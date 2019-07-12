2938fe7ab8ec56403d8f79fe4408fba9aedbd8a6
Drake Foltz <GeenDutchman@byu.net>
2019-07-11 20:04:23 -0400

    Ready for analyzation remotely
    Signed-off-by: Drake Foltz <GeenDutchman@byu.net>

--- analyzer.py
+++ analyzer.py
@@ -3,12 +3,20 @@
 import numpy as np
 import subprocess as sbp
 
-for i in range(100, 102, 1):
-    # this is meant to run a series of values
-    # as a process in the background
-    # also make sure to not run any plates or the movie
+# this is meant to run a series of values
+# as a process in the background
+# also make sure to not run any plates or the movie
+
+star_dist = 200
+star_y_vel = 0.05
+base_bh_sep = 200
+
+# increments in terms of a tenth of the base, up to 2 * the base
+for bh_sep in range(base_bh_sep, base_bh_sep * 5, int(base_bh_sep * 0.1)):
+    star_dist = bh_sep * 50 # double check this scaling
+    star_y_vel = 1 / np.sqrt(star_dist) # for a circular orbit
     result = sbp.run(
-        ["./grScript.sh", "--star", "-x",  "200", "-vy", "0.05" ,"-45", "--tmax", "9e6", "--sep", "-x", (i).__str__()]) #, stdout=sbp.DEVNULL, stderr=sbp.DEVNULL)
+        ["./grScript.sh", "--star", "-x",  str(star_dist), "-vy", str(star_y_vel), "-45", "--omax", "100", "--tmax", "1e12", "--sep", "-x", str(bh_sep)])
 
 # Alert the user that the code has finished
 result = sbp.run(["which", "mail"], stdout=sbp.DEVNULL)

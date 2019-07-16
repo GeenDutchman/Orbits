860252118115b75aa7f321e06f604d4686aa28d3
Drake Foltz <GeenDutchman@byu.net>
2019-07-16 11:19:02 -0400

    Added more code from Prof. Zlochower
    Signed-off-by: Drake Foltz <GeenDutchman@byu.net>

--- PN.py
+++ PN.py
@@ -8,141 +8,284 @@
 import numpy as np
 import RK
 
+
 def Omega_of_r(r, **kwargs):
-  if 'G' in kwargs:
-    G = kwargs['G']
-  else:
-    G = 1.0
+    if 'G' in kwargs:
+        G = kwargs['G']
+    else:
+        G = 1.0
 
-  if 'c_light' in kwargs:
-    c_light = kwargs['c_light']
-  else:
-    c_light = 1.0
+    if 'c_light' in kwargs:
+        c_light = kwargs['c_light']
+    else:
+        c_light = 1.0
 
+    if 'mass' in kwargs:
+        mass = kwargs['mass']
+    else:
+        mass = 1.0
 
-  if 'mass' in kwargs:
-    mass = kwargs['mass']
-  else:
-    mass = 1.0
+    if 'massratio' in kwargs:
+        q = kwargs['massratio']
+    else:
+        q = 1.0
 
-  if 'massratio' in kwargs:
-    q = kwargs['massratio']
-  else:
-    q = 1.0
+    nu = q/(1.0+q)**2  # See Eq. (215). Note, q = m1/m2
 
-  nu = q/(1.0+q)**2 # See Eq. (215). Note, q = m1/m2
+    gamma = G * mass / r / c_light**2  # Eq. (225)
 
-  gamma = G * mass / r / c_light**2 # Eq. (225)
+    # Omega**2 given by Eq. (228)
+    Omega2 = (G * mass / r**3) * (1.0 + (nu - 3.0) * gamma + (6.0
+                                                              + 41.0/4.0 * nu + nu**2) * gamma**2 + (-10 + (-75707./840. +
+                                                                                                            41.0/64. * np.pi**2) * nu + 19.0/2.0 * nu**2 + nu**3) * gamma**3)
 
-  # Omega**2 given by Eq. (228)
-  Omega2 = (G * mass / r**3) * ( 1.0 + (nu - 3.0) * gamma + (6.0
-    +41.0/4.0 * nu + nu**2) * gamma**2 + (-10 + (-75707./840. +
-      41.0/64. * np.pi**2) * nu + 19.0/2.0 * nu**2 + nu**3) * gamma**3)
+    return np.sqrt(Omega2)
 
-  return np.sqrt(Omega2)
 
 def PN_Orbit(t, r, psi, Omega, **kwargs):
-  if 'G' in kwargs:
-    G = kwargs['G']
-  else:
-    G = 1.0
+    if 'G' in kwargs:
+        G = kwargs['G']
+    else:
+        G = 1.0
 
-  if 'c_light' in kwargs:
-    c_light = kwargs['c_light']
-  else:
-    c_light = 1.0
+    if 'c_light' in kwargs:
+        c_light = kwargs['c_light']
+    else:
+        c_light = 1.0
+
+    if 'mass' in kwargs:
+        mass = kwargs['mass']
+    else:
+        mass = 1.0
+
+    if 'massratio' in kwargs:
+        q = kwargs['massratio']
+    else:
+        q = 1.0
+
+    nu = q/(1.0+q)**2  # See Eq. (215). Note, q = m1/m2
+
+    gamma = G * mass / r / c_light**2  # Eq. (225)
+
+    # rdot is given by Eq. (227a)
+    rdot = -64.0 / 5.0 * G**3 * mass**3 * nu / r**3 / c_light**5 * (1.0
+                                                                    + gamma * (-1751.0/336. - 7.0/4.0 * nu))
+
+    # Omega dot is given by Eq. (227b)
+    Omegadot = 96.0/5.0 * G * mass * nu / r**3 * gamma**(5.0/2.) * (1.0
+                                                                    + gamma * (-2591.0/336.0 - 11.0/12.0 * nu))
+
+    # take psi_dot = Omega
+
+    psidot = Omega
+
+    return np.array((rdot, psidot, Omegadot))
 
 
-  if 'mass' in kwargs:
-    mass = kwargs['mass']
-  else:
-    mass = 1.0
+def PN_Acceleration(Xstar, Vstar, rBH, psiBH, OmegaBH, **kwargs):
+    if 'G' in kwargs:
+        G = kwargs['G']
+    else:
+        G = 1.0
 
-  if 'massratio' in kwargs:
-    q = kwargs['massratio']
-  else:
-    q = 1.0
+    if 'c_light' in kwargs:
+        c_light = kwargs['c_light']
+    else:
+        c_light = 1.0
 
-  nu = q/(1.0+q)**2 # See Eq. (215). Note, q = m1/m2
+    if 'mass' in kwargs:
+        mass = kwargs['mass']
+    else:
+        mass = 1.0
 
-  gamma = G * mass / r / c_light**2 # Eq. (225)
+    if 'massratio' in kwargs:
+        q = kwargs['massratio']
+    else:
+        q = 1.0
 
-  # rdot is given by Eq. (227a)
-  rdot = -64.0 / 5.0 * G**3 * mass**3 * nu / r**3 / c_light**5 * (1.0
-      + gamma * (-1751.0/336. - 7.0/4.0 * nu))
+    M1 = mass * q / (1.0 + q)
+    M2 = mass / (1.0 + q)
 
-  # Omega dot is given by Eq. (227b)
-  Omegadot = 96.0/5.0 * G * mass * nu / r**3 * gamma**(5.0/2.) * (1.0
-      + gamma * (-2591.0/336.0 - 11.0/12.0 * nu))
+    Q1 = M1 / mass
+    Q2 = M2 / mass
 
-  # take psi_dot = Omega
+    Delta = Q1 - Q2
 
-  psidot = Omega
+    cospsi = np.cos(psiBH)
+    sinpsi = np.sin(psiBH)
 
-  return np.array((rdot, psidot, Omegadot))
+    nu = q/(1.0+q)**2  # See Eq. (215). Note, q = m1/m2
+
+    gamma = G * mass / rBH / c_light**2  # Eq. (225)
+
+    # rdot is given by Eq. (227a)
+    rdot = -64.0 / 5.0 * G**3 * mass**3 * nu / rBH**3 / c_light**5 * (1.0
+                                                                      + gamma * (-1751.0/336. - 7.0/4.0 * nu))
+
+    # Omega dot is given by Eq. (227b)
+    Omegadot = 96.0/5.0 * G * mass * nu / rBH**3 * gamma**(5.0/2.) * (1.0
+                                                                      + gamma * (-2591.0/336.0 - 11.0/12.0 * nu))
+
+    gammadot = -((G*mass*rdot)/(c_light**2*rBH**2))
+
+    rddot = (7004*G**3*gammadot*mass**3*nu)/(105.*c_light**5*rBH**3) + \
+            (112*G**3*gammadot*mass**3*nu**2)/(5.*c_light**5*rBH**3) + \
+            (192*G**3*mass**3*nu*rdot)/(5.*c_light**5*rBH**4) - \
+            (7004*G**3*gamma*mass**3*nu*rdot)/(35.*c_light**5*rBH**4) - \
+            (336*G**3*gamma*mass**3*nu**2*rdot)/(5.*c_light**5*rBH**4)
+
+    X1 = np.array(
+        [cospsi*(3*Delta*gamma**2*nu + Q2)*rBH, (3*Delta*gamma**2*nu +
+                                                 Q2)*rBH*sinpsi, 0]
+    )
+
+    X2 = np.array(
+        [cospsi*(3*Delta*gamma**2*nu - Q1)*rBH, (3*Delta*gamma**2*nu -
+                                                 Q1)*rBH*sinpsi, 0]
+    )
+
+    V1 = np.array(
+        [6*cospsi*Delta*gamma*gammadot*nu*rBH + cospsi*(3*Delta*gamma**2*nu + Q2)*rdot -
+         Omega*(3*Delta*gamma**2*nu + Q2)*rBH*sinpsi,
+         cospsi*Omega*(3*Delta*gamma**2*nu + Q2)*rBH + 6*Delta*gamma*gammadot*nu*rBH*sinpsi +
+         (3*Delta*gamma**2*nu + Q2)*rdot*sinpsi, 0]
+    )
+
+    V2 = np.array(
+        [6*cospsi*Delta*gamma*gammadot*nu*rBH + cospsi*(3*Delta*gamma**2*nu - Q1)*rdot -
+         Omega*(3*Delta*gamma**2*nu - Q1)*rBH*sinpsi,
+         cospsi*Omega*(3*Delta*gamma**2*nu - Q1)*rBH + 6*Delta*gamma*gammadot*nu*rBH*sinpsi +
+         (3*Delta*gamma**2*nu - Q1)*rdot*sinpsi, 0]
+    )
+
+    A1 = np.array(
+        [6*cospsi*Delta*gammadot**2*nu*rBH - cospsi*Omega**2*(3*Delta*gamma**2*nu + Q2)*rBH +
+         cospsi*(3*Delta*gamma**2*nu + Q2)*rddot + 12*cospsi*Delta*gamma*gammadot*nu*rdot -
+         12*Delta*gamma*gammadot*nu*Omega*rBH*sinpsi - 2*Omega*(3*Delta*gamma**2*nu + Q2)*rdot*sinpsi -
+         (3*Delta*gamma**2*nu + Q2)*rBH*sinpsi*Omegadot,
+         12*cospsi*Delta*gamma*gammadot*nu*Omega*rBH + 2*cospsi*Omega*(3*Delta*gamma**2*nu + Q2)*rdot +
+         6*Delta*gammadot**2*nu*rBH*sinpsi - Omega**2*(3*Delta*gamma**2*nu + Q2)*rBH*sinpsi +
+         (3*Delta*gamma**2*nu + Q2)*rddot*sinpsi + 12*Delta*gamma*gammadot*nu*rdot*sinpsi +
+         cospsi*(3*Delta*gamma**2*nu + Q2)*rBH*Omegadot, 0]
+    )
+
+    A2 = np.array(
+        [6*cospsi*Delta*gammadot**2*nu*rBH - cospsi*Omega**2*(3*Delta*gamma**2*nu - Q1)*rBH +
+         cospsi*(3*Delta*gamma**2*nu - Q1)*rddot + 12*cospsi*Delta*gamma*gammadot*nu*rdot -
+         12*Delta*gamma*gammadot*nu*Omega*rBH*sinpsi - 2*Omega*(3*Delta*gamma**2*nu - Q1)*rdot*sinpsi -
+         (3*Delta*gamma**2*nu - Q1)*rBH*sinpsi*Omegadot,
+         12*cospsi*Delta*gamma*gammadot*nu*Omega*rBH + 2*cospsi*Omega*(3*Delta*gamma**2*nu - Q1)*rdot +
+         6*Delta*gammadot**2*nu*rBH*sinpsi - Omega**2*(3*Delta*gamma**2*nu - Q1)*rBH*sinpsi +
+         (3*Delta*gamma**2*nu - Q1)*rddot*sinpsi + 12*Delta*gamma*gammadot*nu*rdot*sinpsi +
+         cospsi*(3*Delta*gamma**2*nu - Q1)*rBH*Omegadot, 0]
+    )
+
+    X1s = X1 - Xstar
+    X2s = X2 - Xstar
+    X12 = X1 - X2
+
+    n1s = X1s / np.linalg.norm(X1s)
+    n2s = X2s / np.linalg.norm(X2s)
+    n12 = X12 / np.linalg.norm(X12)
+
+    Xs1 = -X1s
+    Xs2 = -X2s
+    X21 = -X12
+
+    ns1 = -n1s
+    ns2 = -n2s
+    n21 = -n12
+
+    rs1 = np.linalg.norm(Xs1)
+    rs2 = np.linalg.norm(Xs2)
+
+    r12 = np.linalg.norm(X12)
+
+    # see https://en.wikipedia.org/wiki/Einstein%E2%80%93Infeld%E2%80%93Hoffmann_equations
+    aNewton = G * M1 * n1s / rs1**2 + G * M2 * n2s / rs2**2
+    aPN1 = (
+        G * M1 * n1s / rs1**2 * (np.dot(Vstar, Vstar) +
+                                 2.0 * np.dot(V1, V1) - 4.0 * np.dot(Vstar, V1) - 1.5 * np.dot(ns1,
+                                                                                               V1)**2 - 4 * G * M1 / rs1 - 4 * G * M2 / rs2 - G * M2 / r12 +
+                                 0.5 * np.dot(X1s, A1))
+        +
+        G * M2 * n2s / rs2**2 * (np.dot(Vstar, Vstar) +
+                                 2.0 * np.dot(V2, V2) - 4.0 * np.dot(Vstar, V2) - 1.5 * np.dot(ns2,
+                                                                                               V2)**2 - 4 * G * M1 / rs1 - 4 * G * M2 / rs2 - G * M1 / r12 +
+                                 0.5 * np.dot(X2s, A2)))
+
+    aPN2 = (
+        G * M1 / rs1**2 * (np.dot(ns1, 4 * Vstar - 3.0 * V1)*(Vstar -
+                                                              V1))
+        +
+        G * M2 / rs2**2 * (np.dot(ns2, 4 * Vstar - 3.0 * V2)*(Vstar -
+                                                              V2)))
+
+    aPN3 = 7.0 / 2.0 * (G * M1 * A1 / rs1 + G * M2 * A2 / rs2)
+
+    return aNewton + (aPN1 + aPN2 + aPN3)/c_light**2, rdot, Omegadot
 
 
 def center_of_mass_coordinates_to_BH_positions(r, psi, **kwargs):
-  if 'G' in kwargs:
-    G = kwargs['G']
-  else:
-    G = 1.0
+    if 'G' in kwargs:
+        G = kwargs['G']
+    else:
+        G = 1.0
 
-  if 'c_light' in kwargs:
-    c_light = kwargs['c_light']
-  else:
-    c_light = 1.0
+    if 'c_light' in kwargs:
+        c_light = kwargs['c_light']
+    else:
+        c_light = 1.0
 
+    if 'mass' in kwargs:
+        mass = kwargs['mass']
+    else:
+        mass = 1.0
 
-  if 'mass' in kwargs:
-    mass = kwargs['mass']
-  else:
-    mass = 1.0
+    if 'massratio' in kwargs:
+        q = kwargs['massratio']
+    else:
+        q = 1.0
 
-  if 'massratio' in kwargs:
-    q = kwargs['massratio']
-  else:
-    q = 1.0
+    nu = q/(1.0+q)**2  # See Eq. (215). Note, q = m1/m2
 
-  nu = q/(1.0+q)**2 # See Eq. (215). Note, q = m1/m2
+    gamma = G * mass / r / c_light**2  # Eq. (225)
 
-  gamma = G * mass / r / c_light**2 # Eq. (225)
+    X1 = q / (1.0 + q)  # X1 = m1/ m
+    X2 = 1.0 / (1.0 + q)  # X2 = m2/m
+    Delta = X1 - X2
 
+    rvec = r * np.array((np.cos(psi), np.sin(psi), 0.0))
+    y1 = rvec * (X2 + 3 * gamma**2 * nu * Delta)  # Eq.(224a)
+    y2 = rvec * (-X1 + 3 * gamma**2 * nu * Delta)  # Eq.(224b)
 
-  X1 = q / (1.0 + q) #  X1 = m1/ m
-  X2 = 1.0 / (1.0 + q) # X2 = m2/m
-  Delta = X1 - X2
+    return y1, y2
 
-  rvec = r * np.array((np.cos(psi), np.sin(psi), 0.0))
-  y1 = rvec * (X2 + 3 * gamma**2 * nu * Delta) # Eq.(224a)
-  y2 = rvec * (-X1 + 3 * gamma**2 * nu * Delta) # Eq.(224b)
-
-  return y1, y2
 
 if __name__ == "__main__":
-  M = 1.0
-  G = 1.0
-  C = 1.0
+    M = 1.0
+    G = 1.0
+    C = 1.0
 
-  R=100.0
+    R = 100.0
 
-  Q = 1.0
+    Q = 1.0
 
-  kwargs={'G' : G, 'mass' : M, 'c_light' : C, 'massratio' : Q,
-      'max_step_size' : 100}
+    kwargs = {'G': G, 'mass': M, 'c_light': C, 'massratio': Q,
+              'max_step_size': 100}
 
-  Omega = Omega_of_r(R, **kwargs)
+    Omega = Omega_of_r(R, **kwargs)
 
-  psi = 0.0
+    psi = 0.0
 
-  y = np.array((R, psi, Omega))
+    y = np.array((R, psi, Omega))
 
-  t = 0.0
+    t = 0.0
 
-  dt = 1.0e-1
-  while y[0] > 10:
-    x1, x2 = center_of_mass_coordinates_to_BH_positions(y[0], y[1], **kwargs)
-    print (t, x1[0] )
+    dt = 1.0e-1
+    while y[0] > 10:
+        x1, x2 = center_of_mass_coordinates_to_BH_positions(
+            y[0], y[1], **kwargs)
+        print(t, x1[0])
 
-    t, y, dt = RK.RK45_Step(t, y, dt, PN_Orbit, **kwargs)
-
+        t, y, dt = RK.RK45_Step(t, y, dt, PN_Orbit, **kwargs)

dfc74b8aa765b43d5f2885e84ef3757474c2e96c
Drake Foltz <GeenDutchman@byu.net>
2019-07-26 10:10:32 -0400

    Updated getOrbits filenames and overlay
    Signed-off-by: Drake Foltz <GeenDutchman@byu.net>

--- getOrbits.py
+++ getOrbits.py
@@ -4,8 +4,15 @@ import numpy as np
 
 def main(argv):
     filename = argv[0]
-    outfile = argv[1]
-    orbits_to_get = argv[2:]
+    overlay = False
+    print(argv[1])
+    if argv[1] == '-ov' or argv[1] == '--overlay':
+        overlay = True
+        orbits_to_get = argv[2:]
+        print('Calculating overlay.')
+    else:
+        orbits_to_get = argv[1:]
+    
 
     data = np.genfromtxt(filename, names=True)
 
@@ -24,11 +31,30 @@ def main(argv):
     for s in data.dtype.names:
         header_text += s + ' '
 
-    for orbit_num in orbits_to_get:
-        outfile_name = 'o' + str(orbit_num) + outfile
+    for orbit_num_str in orbits_to_get:
+        orbit_num_int = int(orbit_num_str)
+
+        outfile_name = "" # name it differently
+        if orbit_num_int < 0: # no negative indecies
+            orbit_num_int = len(orbit_list) + orbit_num_int
+            outfile_name = str(orbit_num_int) + filename
+        else:
+            outfile_name = orbit_num_str + filename
+
+        print('Getting orbit number', orbit_num_int)
+
+        # name it differently for an overlay
+        if overlay is True:
+            outfile_name = 'ov' + outfile_name
+        else:
+            outfile_name = 'o' + outfile_name
+
         print('Trying to write to', outfile_name, end='')
-        begin, end = orbit_list[int(orbit_num)]
+
+        begin, end = orbit_list[orbit_num_int]
         sub_data = data[begin:end + 1]
+        if overlay is True and orbit_num_int is not 0: # shift it to overlay
+            sub_data['star_angle'] = [x - (2 * np.pi * orbit_num_int) for x in sub_data['star_angle']]
         np.savetxt(outfile_name, sub_data, header=header_text)
         print('...succeeded')
 
